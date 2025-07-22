d70230e65f7a699a23bcdbf5466091fb2cf833e6
Khoshbin <xoshbin@gmail.com>
2025-07-23 00:19:29 +0300

    feat(migrations): Add multiple migration files for companies, users, partners, fiscal positions, taxes, and products

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->foreignId('company_id')->nullable()->after('id')->constrained('companies');
+            // $table->foreignId('company_id')->nullable()->constrained('companies');
             $table->timestamps();
         });
 
