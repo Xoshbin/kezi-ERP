5e41963644df7347d76a5291f7fc537dcea1748a
Khoshbin <xoshbin@gmail.com>
2025-07-22 15:46:46 +0300

    feat(journal-entry): Add PeriodIsLockedException and validation for locked periods in journal entry creation

--- app/Models/LockDate.php
+++ app/Models/LockDate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -25,6 +26,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class LockDate extends Model
 {
+    use HasFactory;
     /**
      * The database table associated with the model.
      *
