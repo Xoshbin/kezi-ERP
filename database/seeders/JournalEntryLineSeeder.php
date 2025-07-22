97361c86c370829c2a4f1ff8a773d148b9b7e3b3
Khoshbin <xoshbin@gmail.com>
2025-07-22 23:54:44 +0300

    feat(seeders): Add initial seeders for various database models

---
+++ database/seeders/JournalEntryLineSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JournalEntryLineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
---
+++ database/seeders/VendorBillLineSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VendorBillLineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
