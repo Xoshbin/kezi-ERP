45234d3de6458136daa4a3a391a7d6e592be14a2
Khoshbin <xoshbin@gmail.com>
2025-07-27 00:03:07 +0300

    feat(exceptions): Enhance DeletionNotAllowedException and UpdateNotAllowedException with detailed constructors and render methods

--- app/Exceptions/UpdateNotAllowedException.php
+++ app/Exceptions/UpdateNotAllowedException.php
@@ -2,9 +2,40 @@
 
 namespace App\Exceptions;
 
-use Exception; // It's good practice to import the base class
+use Exception;
+use Throwable;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse; // Alias for consistency
 
 class UpdateNotAllowedException extends Exception
 {
+    /**
+     * Create a new update not allowed exception instance.
+     *
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     * @return void
+     */
+    public function __construct(string $message = "Modification of this financial record is not allowed due to accounting principles.", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return SymfonyResponse // Changed the return type hint here
+     */
+    public function render($request): SymfonyResponse
+    {
+        if ($request->expectsJson()) {
+            return response()->json([
+                'message' => $this->getMessage(),
+                'error_code' => 'ACCOUNTING_UPDATE_PROHIBITED',
+            ], 403); // 403 Forbidden
+        }
+
+        return back()->with('error', $this->getMessage());
+    }
 }
