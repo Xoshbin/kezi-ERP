2d5b89d88098d35b8ef6cd48d9c4056d6b3da9e2
Khoshbin <xoshbin@gmail.com>
2025-07-30 10:52:12 +0300

    feat(JournalResource): Add localization support for journal fields in English and Kurdish

---
+++ resources/lang/en/journal.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    // JournalResource.php
+    'default_debit_account' => 'Default Debit Account',
+    'default_debit_account_helper' => 'For Bank/Cash journals, this is the bank account to use for payments.',
+    'default_credit_account' => 'Default Credit Account',
+    'default_credit_account_helper' => 'For Bank/Cash journals, this is the bank account to use for payments.',
+    'default_debit_account_short' => 'Default Debit Acct.',
+    'default_credit_account_short' => 'Default Credit Acct.',
+
+    // JournalEntriesRelationManager.php
+    'entry_date' => 'Entry Date',
+    'reference' => 'Reference',
+    'description' => 'Description',
+    'is_posted' => 'Posted',
+    'journal_entries' => 'Journal Entries',
+];
