33ad046974a49e05c9c4d3daa5a82bebbcb72cca
Khoshbin <xoshbin@gmail.com>
2025-07-30 10:40:59 +0300

    feat(CompanyResource): Add localization support for company labels and fields in English and Kurdish

--- app/Filament/Resources/CompanyResource/RelationManagers/AccountsRelationManager.php
+++ app/Filament/Resources/CompanyResource/RelationManagers/AccountsRelationManager.php
@@ -8,26 +8,33 @@ use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class AccountsRelationManager extends RelationManager
 {
     protected static string $relationship = 'accounts';
 
+    protected static ?string $title = 'company.accounts.title';
+
     public function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('code')
+                    ->label(__('company.accounts.code'))
                     ->required()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('name')
+                    ->label(__('company.accounts.name'))
                     ->required()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('type')
+                    ->label(__('company.accounts.type'))
                     ->required()
                     ->maxLength(255),
                 Forms\Components\Toggle::make('is_deprecated')
+                    ->label(__('company.accounts.is_deprecated'))
                     ->required(),
             ]);
     }
@@ -37,10 +44,15 @@ class AccountsRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('name')
             ->columns([
-                Tables\Columns\TextColumn::make('code'),
-                Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('type'),
-                Tables\Columns\IconColumn::make('is_deprecated')->boolean(),
+                Tables\Columns\TextColumn::make('code')
+                    ->label(__('company.accounts.code')),
+                Tables\Columns\TextColumn::make('name')
+                    ->label(__('company.accounts.name')),
+                Tables\Columns\TextColumn::make('type')
+                    ->label(__('company.accounts.type')),
+                Tables\Columns\IconColumn::make('is_deprecated')
+                    ->label(__('company.accounts.is_deprecated'))
+                    ->boolean(),
             ])
             ->filters([
                 //
