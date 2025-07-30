d9723306d2d188b78a53f3d8388808a12a176400
Khoshbin <xoshbin@gmail.com>
2025-07-30 11:06:34 +0300

    feat(UserResource): Add localization support for user fields and pages in English and Kurdish

---
+++ resources/lang/ku/user.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    // General
+    'singular' => 'بەکارهێنەر',
+    'plural' => 'بەکارهێنەران',
+
+    // Forms
+    'form' => [
+        'company' => [
+            'label' => 'کۆمپانیا',
+        ],
+        'name' => [
+            'label' => 'ناو',
+        ],
+        'email' => [
+            'label' => 'ئیمەیڵ',
+        ],
+        'email_verified_at' => [
+            'label' => 'ئیمەیڵ پشتڕاستکرایەوە لە',
+        ],
+        'password' => [
+            'label' => 'وشەی نهێنی',
+        ],
+    ],
+
+    // Columns
+    'column' => [
+        'company' => [
+            'name' => 'کۆمپانیا',
+        ],
+        'name' => 'ناو',
+        'email' => 'ئیمەیڵ',
+        'email_verified_at' => 'ئیمەیڵ پشتڕاستکرایەوە لە',
+        'created_at' => 'دروستکرا لە',
+        'updated_at' => 'نوێکرایەوە لە',
+    ],
+
+    // Pages
+    'page' => [
+        'index' => [
+            'title' => 'بەکارهێنەران',
+        ],
+        'create' => [
+            'title' => 'دروستکردنی بەکارهێنەر',
+        ],
+        'edit' => [
+            'title' => 'دەستکاری بەکارهێنەر',
+        ],
+    ],
+];
