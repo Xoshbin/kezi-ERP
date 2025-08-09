fc3671bf218b3fc7733bf70230f1dedb64762869
Khoshbin <xoshbin@gmail.com>
2025-08-10 00:38:07 +0300

    preparing for deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,11 +23,11 @@ class DatabaseSeeder extends Seeder
                 JournalSeeder::class,
                 UpdateCompanyDefaultsSeeder::class,
                 UserSeeder::class,
-                JournalEntrySeeder::class,
+                // JournalEntrySeeder::class,
 
                 // 3. Operational data
                 // Basic data for transactions (customers, vendors, products).
-                PartnerSeeder::class,
+                // PartnerSeeder::class,
                 // TaxSeeder::class,
                 // ProductSeeder::class,
 
@@ -52,10 +52,10 @@ class DatabaseSeeder extends Seeder
 
                 // 7. Financial documents
                 // The primary transactional records.
-                 VendorBillSeeder::class,
-                 InvoiceSeeder::class,
+                //  VendorBillSeeder::class,
+                //  InvoiceSeeder::class,
                 // InvoiceLineSeeder::class, // Removed as logic is now in InvoiceSeeder
-                PaymentSeeder::class,
+                // PaymentSeeder::class,
                 // PaymentDocumentLinkSeeder::class,
 
                 // 8. Adjustments and statements
