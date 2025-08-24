39e36d02484cdc8f185e5a308789bc9e44d323d4
Khoshbin <xoshbin@gmail.com>
2025-08-24 21:39:28 +0300

    feat: refactor all HR resource schemas to follow VendorBill patterns with comprehensive multi-language support

--- app/Filament/Clusters/HumanResources/Resources/Employees/Schemas/EmployeeForm.php
+++ app/Filament/Clusters/HumanResources/Resources/Employees/Schemas/EmployeeForm.php
@@ -2,71 +2,255 @@
 
 namespace App\Filament\Clusters\HumanResources\Resources\Employees\Schemas;
 
+use App\Filament\Support\TranslatableSelect;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class EmployeeForm
 {
     public static function configure(Schema $schema): Schema
     {
-        return $schema
-            ->components([
-                Select::make('company_id')
-                    ->relationship('company', 'name')
-                    ->required(),
-                Select::make('user_id')
-                    ->relationship('user', 'name'),
-                Select::make('department_id')
-                    ->relationship('department', 'name'),
-                Select::make('position_id')
-                    ->relationship('position', 'title'),
-                Select::make('manager_id')
-                    ->relationship('manager', 'id'),
-                TextInput::make('employee_number')
-                    ->required(),
-                TextInput::make('first_name')
-                    ->required(),
-                TextInput::make('last_name')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                TextInput::make('phone')
-                    ->tel(),
-                DatePicker::make('date_of_birth'),
-                TextInput::make('gender'),
-                TextInput::make('marital_status'),
-                TextInput::make('nationality'),
-                TextInput::make('national_id'),
-                TextInput::make('passport_number'),
-                TextInput::make('address_line_1'),
-                TextInput::make('address_line_2'),
-                TextInput::make('city'),
-                TextInput::make('state'),
-                TextInput::make('zip_code'),
-                TextInput::make('country'),
-                TextInput::make('emergency_contact_name'),
-                TextInput::make('emergency_contact_phone')
-                    ->tel(),
-                TextInput::make('emergency_contact_relationship'),
-                DatePicker::make('hire_date')
-                    ->required(),
-                DatePicker::make('termination_date'),
-                TextInput::make('employment_status')
-                    ->required()
-                    ->default('active'),
-                TextInput::make('employee_type')
-                    ->required()
-                    ->default('full_time'),
-                TextInput::make('bank_name'),
-                TextInput::make('bank_account_number'),
-                TextInput::make('bank_routing_number'),
-                Toggle::make('is_active')
-                    ->required(),
-            ]);
+        return $schema->components([
+            Section::make(__('employee.basic_information'))
+                ->description(__('employee.basic_information_description'))
+                ->schema([
+                    TextInput::make('employee_number')
+                        ->label(__('employee.employee_number'))
+                        ->required()
+                        ->unique(ignoreRecord: true)
+                        ->maxLength(50)
+                        ->columnSpan(1),
+
+                    TextInput::make('first_name')
+                        ->label(__('employee.first_name'))
+                        ->required()
+                        ->maxLength(255)
+                        ->columnSpan(1),
+
+                    TextInput::make('last_name')
+                        ->label(__('employee.last_name'))
+                        ->required()
+                        ->maxLength(255)
+                        ->columnSpan(1),
+
+                    TextInput::make('email')
+                        ->label(__('employee.email'))
+                        ->email()
+                        ->required()
+                        ->unique(ignoreRecord: true)
+                        ->maxLength(255)
+                        ->columnSpan(2),
+
+                    TextInput::make('phone')
+                        ->label(__('employee.phone'))
+                        ->tel()
+                        ->maxLength(20)
+                        ->columnSpan(1),
+                ])
+                ->columns(3)
+                ->columnSpanFull(),
+
+            Section::make(__('employee.organizational_details'))
+                ->description(__('employee.organizational_details_description'))
+                ->schema([
+                    TranslatableSelect::make('department_id', \App\Models\Department::class, __('employee.department'))
+                        ->columnSpan(1),
+
+                    TranslatableSelect::make('position_id', \App\Models\Position::class, __('employee.position'))
+                        ->columnSpan(1),
+
+                    TranslatableSelect::standard(
+                        'manager_id',
+                        \App\Models\Employee::class,
+                        ['first_name', 'last_name', 'employee_number'],
+                        __('employee.manager')
+                    )
+                        ->columnSpan(1),
+
+                    TranslatableSelect::standard(
+                        'user_id',
+                        \App\Models\User::class,
+                        ['name', 'email'],
+                        __('employee.user_account')
+                    )
+                        ->columnSpan(1),
+                ])
+                ->columns(2)
+                ->columnSpanFull(),
+
+            Section::make(__('employee.personal_information'))
+                ->description(__('employee.personal_information_description'))
+                ->schema([
+                    DatePicker::make('date_of_birth')
+                        ->label(__('employee.date_of_birth'))
+                        ->maxDate(now()->subYears(16))
+                        ->columnSpan(1),
+
+                    Select::make('gender')
+                        ->label(__('employee.gender'))
+                        ->options([
+                            'male' => __('employee.gender_male'),
+                            'female' => __('employee.gender_female'),
+                            'other' => __('employee.gender_other'),
+                        ])
+                        ->columnSpan(1),
+
+                    Select::make('marital_status')
+                        ->label(__('employee.marital_status'))
+                        ->options([
+                            'single' => __('employee.marital_status_single'),
+                            'married' => __('employee.marital_status_married'),
+                            'divorced' => __('employee.marital_status_divorced'),
+                            'widowed' => __('employee.marital_status_widowed'),
+                        ])
+                        ->columnSpan(1),
+
+                    TextInput::make('nationality')
+                        ->label(__('employee.nationality'))
+                        ->maxLength(100)
+                        ->columnSpan(1),
+
+                    TextInput::make('national_id')
+                        ->label(__('employee.national_id'))
+                        ->maxLength(50)
+                        ->columnSpan(1),
+
+                    TextInput::make('passport_number')
+                        ->label(__('employee.passport_number'))
+                        ->maxLength(50)
+                        ->columnSpan(1),
+                ])
+                ->columns(3)
+                ->columnSpanFull(),
+
+            Section::make(__('employee.address_information'))
+                ->description(__('employee.address_information_description'))
+                ->schema([
+                    TextInput::make('address_line_1')
+                        ->label(__('employee.address_line_1'))
+                        ->maxLength(255)
+                        ->columnSpan(2),
+
+                    TextInput::make('address_line_2')
+                        ->label(__('employee.address_line_2'))
+                        ->maxLength(255)
+                        ->columnSpan(1),
+
+                    TextInput::make('city')
+                        ->label(__('employee.city'))
+                        ->maxLength(100)
+                        ->columnSpan(1),
+
+                    TextInput::make('state')
+                        ->label(__('employee.state'))
+                        ->maxLength(100)
+                        ->columnSpan(1),
+
+                    TextInput::make('zip_code')
+                        ->label(__('employee.zip_code'))
+                        ->maxLength(20)
+                        ->columnSpan(1),
+
+                    TextInput::make('country')
+                        ->label(__('employee.country'))
+                        ->maxLength(100)
+                        ->columnSpan(1),
+                ])
+                ->columns(3)
+                ->columnSpanFull(),
+
+            Section::make(__('employee.emergency_contact'))
+                ->description(__('employee.emergency_contact_description'))
+                ->schema([
+                    TextInput::make('emergency_contact_name')
+                        ->label(__('employee.emergency_contact_name'))
+                        ->maxLength(255)
+                        ->columnSpan(1),
+
+                    TextInput::make('emergency_contact_phone')
+                        ->label(__('employee.emergency_contact_phone'))
+                        ->tel()
+                        ->maxLength(20)
+                        ->columnSpan(1),
+
+                    TextInput::make('emergency_contact_relationship')
+                        ->label(__('employee.emergency_contact_relationship'))
+                        ->maxLength(100)
+                        ->columnSpan(1),
+                ])
+                ->columns(3)
+                ->columnSpanFull(),
+
+            Section::make(__('employee.employment_details'))
+                ->description(__('employee.employment_details_description'))
+                ->schema([
+                    DatePicker::make('hire_date')
+                        ->label(__('employee.hire_date'))
+                        ->required()
+                        ->maxDate(now())
+                        ->columnSpan(1),
+
+                    DatePicker::make('termination_date')
+                        ->label(__('employee.termination_date'))
+                        ->minDate(fn ($get) => $get('hire_date'))
+                        ->columnSpan(1),
+
+                    Select::make('employment_status')
+                        ->label(__('employee.employment_status'))
+                        ->options([
+                            'active' => __('employee.employment_status_active'),
+                            'inactive' => __('employee.employment_status_inactive'),
+                            'terminated' => __('employee.employment_status_terminated'),
+                            'on_leave' => __('employee.employment_status_on_leave'),
+                        ])
+                        ->required()
+                        ->default('active')
+                        ->columnSpan(1),
+
+                    Select::make('employee_type')
+                        ->label(__('employee.employee_type'))
+                        ->options([
+                            'full_time' => __('employee.employee_type_full_time'),
+                            'part_time' => __('employee.employee_type_part_time'),
+                            'contract' => __('employee.employee_type_contract'),
+                            'intern' => __('employee.employee_type_intern'),
+                        ])
+                        ->required()
+                        ->default('full_time')
+                        ->columnSpan(1),
+
+                    Toggle::make('is_active')
+                        ->label(__('employee.is_active'))
+                        ->default(true)
+                        ->columnSpan(1),
+                ])
+                ->columns(3)
+                ->columnSpanFull(),
+
+            Section::make(__('employee.banking_information'))
+                ->description(__('employee.banking_information_description'))
+                ->schema([
+                    TextInput::make('bank_name')
+                        ->label(__('employee.bank_name'))
+                        ->maxLength(255)
+                        ->columnSpan(1),
+
+                    TextInput::make('bank_account_number')
+                        ->label(__('employee.bank_account_number'))
+                        ->maxLength(50)
+                        ->columnSpan(1),
+
+                    TextInput::make('bank_routing_number')
+                        ->label(__('employee.bank_routing_number'))
+                        ->maxLength(50)
+                        ->columnSpan(1),
+                ])
+                ->columns(3)
+                ->columnSpanFull(),
+        ]);
     }
 }
---
+++ resources/lang/ar/department.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    // Basic Information
+    'basic_information' => 'المعلومات الأساسية',
+    'basic_information_description' => 'حدد التفاصيل الأساسية والهيكل لهذا القسم',
+    'name' => 'اسم القسم',
+    'parent_department' => 'القسم الرئيسي',
+    'description' => 'الوصف',
+
+    // Management
+    'management' => 'الإدارة',
+    'management_description' => 'حدد المدير والحالة لهذا القسم',
+    'manager' => 'مدير القسم',
+    'is_active' => 'نشط',
+
+    // Navigation
+    'navigation_label' => 'الأقسام',
+    'navigation_group' => 'الموارد البشرية',
+];
