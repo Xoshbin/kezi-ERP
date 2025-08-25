9c6a963c057298929df8eef9508e35aec8a4f235
Khoshbin <xoshbin@gmail.com>
2025-08-25 09:48:21 +0300

    add Translatable traits to HR resource pages to enable proper translatable field functionality

--- app/Filament/Clusters/HumanResources/Resources/Employees/Pages/EditEmployee.php
+++ app/Filament/Clusters/HumanResources/Resources/Employees/Pages/EditEmployee.php
@@ -7,14 +7,19 @@ use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
 use Filament\Resources\Pages\EditRecord;
+use LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns\Translatable;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
 
 class EditEmployee extends EditRecord
 {
+    use Translatable;
+
     protected static string $resource = EmployeeResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            LocaleSwitcher::make(),
             DeleteAction::make(),
             ForceDeleteAction::make(),
             RestoreAction::make(),
--- app/Filament/Clusters/HumanResources/Resources/LeaveTypes/Pages/EditLeaveType.php
+++ app/Filament/Clusters/HumanResources/Resources/LeaveTypes/Pages/EditLeaveType.php
@@ -5,14 +5,19 @@ namespace App\Filament\Clusters\HumanResources\Resources\LeaveTypes\Pages;
 use App\Filament\Clusters\HumanResources\Resources\LeaveTypes\LeaveTypeResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns\Translatable;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
 
 class EditLeaveType extends EditRecord
 {
+    use Translatable;
+
     protected static string $resource = LeaveTypeResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            LocaleSwitcher::make(),
             DeleteAction::make(),
         ];
     }
