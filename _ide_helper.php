e2196d8125b1542935026104a24faeafe9c1a50d
Khoshbin <xoshbin@gmail.com>
2025-08-29 09:58:13 +0300

    Remove custome filament theme

--- _ide_helper.php
+++ _ide_helper.php
@@ -32175,11 +32175,6 @@ namespace  {
 }
 
 
-namespace Facades\Livewire\Features\SupportFileUploads {
-    /**
-     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
-    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
-}
 
 
 
