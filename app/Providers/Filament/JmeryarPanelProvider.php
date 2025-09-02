e8ccd5099b9b7461997612cb17047ea44c1df696
Khoshbin <xoshbin@gmail.com>
2025-09-02 10:13:14 +0300

    Delete theme plugin

--- app/Providers/Filament/JmeryarPanelProvider.php
+++ app/Providers/Filament/JmeryarPanelProvider.php
@@ -67,7 +67,6 @@ class JmeryarPanelProvider extends PanelProvider
             ->tenantRegistration(RegisterCompany::class)
             ->tenantProfile(EditCompanyProfile::class)
             ->plugins([
-                JmeryarTheme::make(),
                 SpatieTranslatablePlugin::make()
                     ->defaultLocales(['en', 'ckb', 'ar']),
                 FilamentAiHelperPlugin::make()
