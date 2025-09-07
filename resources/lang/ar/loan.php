74c03358ac19de621a411bb071fa34282ba164f0
Khoshbin <xoshbin@gmail.com>
2025-09-07 14:51:43 +0300

    Refactor LoanAgreement form to compact sections and patterns

---
+++ resources/lang/ar/loan.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'form' => [
+        'counterparty_currency' => 'الطرف المقابل والعملة',
+        'partner' => 'الشريك',
+        'name' => 'اسم القرض',
+        'loan_type' => 'نوع القرض',
+        'loan_date' => 'تاريخ القرض',
+        'start_date' => 'تاريخ البدء',
+        'maturity_date' => 'تاريخ الاستحقاق',
+        'duration_months' => 'المدة (بالأشهر)',
+        'months' => 'أشهر',
+        'currency' => 'العملة',
+        'principal_amount' => 'قيمة الأصل',
+        'outstanding_principal' => 'الأصل المستحق',
+        'schedule_rates' => 'الجدول والأسعار',
+        'schedule_method' => 'طريقة الجدولة',
+        'interest_rate' => 'المعدل السنوي الاسمي',
+        'eir_enabled' => 'استخدام معدل الفعلي EIR',
+        'eir_rate' => 'معدل EIR الدوري',
+        'status' => 'الحالة',
+    ],
+];
+
