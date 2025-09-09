c28d762be99516e75769dc8da26ad00e74500a7a
Khoshbin <xoshbin@gmail.com>
2025-09-09 21:06:21 +0300

    Fix css styles and javascripts of the pertuk package

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -8,8 +9,17 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/filament/jmeryar/theme.js",
+                "packages/xoshbin/pertuk/resources/js/pertuk.js",
             ],
             refresh: true,
+        }),
+        tailwindcss({
+            content: [
+                "./resources/**/*.blade.php",
+                "./resources/**/*.js",
+                "./packages/xoshbin/pertuk/resources/views/**/*.blade.php",
+                "./packages/xoshbin/pertuk/resources/css/**/*.css",
+            ],
         }),
     ],
 });
