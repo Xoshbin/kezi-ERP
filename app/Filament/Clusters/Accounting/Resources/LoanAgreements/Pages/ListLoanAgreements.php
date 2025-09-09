238252cd9c3977151e0ea15edef9204b32c8a654
Khoshbin <xoshbin@gmail.com>
2025-09-09 17:22:17 +0300

    Move documentation system to reusable pertuk package

--- app/Filament/Clusters/Accounting/Resources/LoanAgreements/Pages/ListLoanAgreements.php
+++ app/Filament/Clusters/Accounting/Resources/LoanAgreements/Pages/ListLoanAgreements.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Clusters\Accounting\Resources\LoanAgreements\Pages;
 
 use App\Filament\Clusters\Accounting\Resources\LoanAgreements\LoanAgreementResource;
-use App\Support\Filament\DocsAction;
+use Xoshbin\Pertuk\Support\DocsAction;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
