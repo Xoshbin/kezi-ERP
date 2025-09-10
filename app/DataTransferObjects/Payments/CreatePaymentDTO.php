ef13cd9611d76b304e747c016240eae4fe33adcd
Khoshbin <xoshbin@gmail.com>
2025-09-10 15:27:07 +0300

    Fix prepopulating partner_id select in paymentresource

--- app/DataTransferObjects/Payments/CreatePaymentDTO.php
+++ app/DataTransferObjects/Payments/CreatePaymentDTO.php
@@ -18,7 +18,7 @@ class CreatePaymentDTO
         public readonly string $payment_date,
         public readonly PaymentType $payment_type,
         public readonly PaymentMethod $payment_method,
-        public readonly ?int $partner_id,
+        public readonly ?int $paid_to_from_partner_id,
         public readonly ?Money $amount,
         public readonly array $document_links,
         public readonly ?string $reference,
