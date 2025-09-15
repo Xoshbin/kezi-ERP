538c0eb45ad012dbd2cd7219e3b3ff5242617086
Khoshbin <xoshbin@gmail.com>
2025-09-15 21:47:47 +0300

    Add missing company_id mutations to Filament create pages
    Signed-off-by: Khoshbin <xoshbin@gmail.com>

--- app/Filament/Clusters/Settings/Resources/Accounts/Pages/CreateAccount.php
+++ app/Filament/Clusters/Settings/Resources/Accounts/Pages/CreateAccount.php
@@ -4,6 +4,7 @@ namespace App\Filament\Clusters\Settings\Resources\Accounts\Pages;
 
 use App\Filament\Clusters\Settings\Resources\Accounts\AccountResource;
 use App\Services\AccountService;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
 use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
@@ -25,6 +26,14 @@ class CreateAccount extends CreateRecord
     public function getTitle(): string
     {
         return __('filament.actions.create').' '.__('account.label');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $tenant = Filament::getTenant();
+        $data['company_id'] = $tenant?->getKey() ?? 0;
+
+        return $data;
     }
 
     protected function handleRecordCreation(array $data): Model
