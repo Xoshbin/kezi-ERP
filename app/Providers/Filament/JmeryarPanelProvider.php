4363a18c554e39519864dc49c7db3f7503e95f54
Khoshbin <xoshbin@gmail.com>
2025-09-17 23:48:14 +0300

    Add Filament Developer Logins plugin and update panel provider

--- app/Providers/Filament/JmeryarPanelProvider.php
+++ app/Providers/Filament/JmeryarPanelProvider.php
@@ -2,28 +2,29 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Pages\Tenancy\EditCompanyProfile;
-use App\Filament\Pages\Tenancy\RegisterCompany;
-use App\Models\Company;
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\AuthenticateSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
-use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
-use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
-use Xoshbin\CustomFields\CustomFieldsPlugin;
-use Xoshbin\FilamentAiHelper\FilamentAiHelperPlugin;
+use App\Models\Company;
+use Filament\PanelProvider;
+use Filament\Pages\Dashboard;
+use Filament\Support\Colors\Color;
 use Xoshbin\JmeryarTheme\JmeryarTheme;
+use Filament\Http\Middleware\Authenticate;
+use Xoshbin\CustomFields\CustomFieldsPlugin;
+use App\Filament\Pages\Tenancy\RegisterCompany;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Filament\Http\Middleware\AuthenticateSession;
+use App\Filament\Pages\Tenancy\EditCompanyProfile;
+use Xoshbin\FilamentAiHelper\FilamentAiHelperPlugin;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use DutchCodingCompany\FilamentDeveloperLogins\FilamentDeveloperLoginsPlugin;
 
 class JmeryarPanelProvider extends PanelProvider
 {
@@ -72,6 +73,11 @@ class JmeryarPanelProvider extends PanelProvider
                 CustomFieldsPlugin::make(),
                 SpatieTranslatablePlugin::make()
                     ->defaultLocales(['en', 'ckb', 'ar']),
+                FilamentDeveloperLoginsPlugin::make()
+                    ->enabled(app()->environment('local'))
+                    ->users([
+                        'Admin' => 'admin@jmeryar.com',
+                    ])
                 // FilamentAiHelperPlugin::make()
                 //     ->buttonLabel('AccounTech Pro')
                 //     ->buttonIcon('heroicon-o-sparkles')
