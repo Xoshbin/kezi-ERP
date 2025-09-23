ac530d20400a04249c1d5c84dce4a39089fc90d1
Khoshbin <xoshbin@gmail.com>
2025-09-23 21:34:27 +0300

    Fix currency factory race conditions in parallel tests

--- database/factories/LoanAgreementFactory.php
+++ database/factories/LoanAgreementFactory.php
@@ -28,7 +28,7 @@ class LoanAgreementFactory extends Factory
             'start_date' => now()->addMonth()->startOfMonth()->toDateString(),
             'maturity_date' => now()->addMonths(12)->toDateString(),
             'duration_months' => 12,
-            'currency_id' => Currency::factory(),
+            'currency_id' => Currency::factory()->createSafely(),
             'principal_amount' => Money::of('10000', 'USD'),
             'outstanding_principal' => Money::of('10000', 'USD'),
             'loan_type' => LoanType::Payable,
