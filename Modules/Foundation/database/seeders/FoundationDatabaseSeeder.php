9248891516d66db045231d72ab492ab3f8b03e2e
Khoshbin <xoshbin@gmail.com>
2025-09-27 13:55:54 +0300

    Created base modules

---
+++ Modules/Foundation/database/seeders/FoundationDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Foundation\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FoundationDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
