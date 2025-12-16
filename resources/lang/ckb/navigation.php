050ec09f9c6a2b5faeb079f2365fd598e03b0098
Xoshbin <xoshbin@gmail.com>
2025-12-16 20:41:36 +0300

    feat: Add missin Kurdish Sorani and Arabic translations for company, enums, navigation, and user modules.

--- resources/lang/ckb/navigation.php
+++ resources/lang/ckb/navigation.php
@@ -5,6 +5,9 @@ return [
         'sales_purchases' => 'کڕین و فرۆشتن',
         'core_accounting' => 'ژمێریاری سەرەکی',
         'banking_cash' => 'بانک و نەقد',
+        'inventory' => 'Inventory Management',
+        'financial_planning' => 'Financial Planning',
+        'reports' => 'Reports',
     ],
     'clusters' => [
         'inventory' => 'کۆگا',
