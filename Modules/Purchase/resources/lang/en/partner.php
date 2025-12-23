fef68ed49a64a7adff2ab939f85f5168bed98de3
Xoshbin <xoshbin@gmail.com>
2025-12-23 22:36:06 +0300

    refactor(locales): make partner locales independent per module

---
+++ Modules/Purchase/resources/lang/en/partner.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // Partner creation labels
+    'create' => 'Create',
+    'create_customer' => 'Create Customer',
+    'create_vendor' => 'Create Vendor',
+
+    // Type labels
+    'type' => 'Type',
+];
