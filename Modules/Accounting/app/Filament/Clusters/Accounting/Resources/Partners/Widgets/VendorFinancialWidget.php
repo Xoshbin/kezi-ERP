551cff9cca4e395855a0e6c3aea25c459290d2bb
Xoshbin <xoshbin@gmail.com>
2025-12-24 00:19:29 +0300

    refactor: Update translation keys to use the 'accounting::' namespace in financial widgets.

--- Modules/Accounting/app/Filament/Clusters/Accounting/Resources/Partners/Widgets/VendorFinancialWidget.php
+++ Modules/Accounting/app/Filament/Clusters/Accounting/Resources/Partners/Widgets/VendorFinancialWidget.php
@@ -33,8 +33,8 @@ class VendorFinancialWidget extends BaseWidget
         $stats = [];
 
         // Total To Pay - like in image
-        $overdueText = $overdueBalance->isZero() ? '' : __('partner.widgets.includes_overdue', ['amount' => $overdueBalance->formatTo('en_US')]);
-        $stats[] = Stat::make(__('partner.widgets.total_to_pay'), $outstandingBalance->formatTo('en_US'))
+        $overdueText = $overdueBalance->isZero() ? '' : __('accounting::partner.widgets.includes_overdue', ['amount' => $overdueBalance->formatTo('en_US')]);
+        $stats[] = Stat::make(__('accounting::partner.widgets.total_to_pay'), $outstandingBalance->formatTo('en_US'))
             ->description($overdueText)
             ->descriptionIcon('heroicon-m-credit-card')
             ->color($outstandingBalance->isZero() ? 'gray' : 'danger')
@@ -43,8 +43,8 @@ class VendorFinancialWidget extends BaseWidget
             ]);
 
         // Due Within 7 Days - like in image
-        $stats[] = Stat::make(__('partner.widgets.due_within_7_days'), $dueIn7Days->formatTo('en_US'))
-            ->description(__('partner.widgets.urgent_payments'))
+        $stats[] = Stat::make(__('accounting::partner.widgets.due_within_7_days'), $dueIn7Days->formatTo('en_US'))
+            ->description(__('accounting::partner.widgets.urgent_payments'))
             ->descriptionIcon('heroicon-m-exclamation-triangle')
             ->color($dueIn7Days->isZero() ? 'gray' : 'warning')
             ->extraAttributes([
@@ -56,8 +56,8 @@ class VendorFinancialWidget extends BaseWidget
         //                    ($avgPaymentDays <= 30 ? 'success' :
         //                    ($avgPaymentDays <= 60 ? 'warning' : 'danger'));
 
-        // $stats[] = Stat::make(__('partner.widgets.average_payment_time'), $avgPaymentDays . ' ' . __('partner.widgets.days'))
-        //     ->description(__('partner.widgets.our_payment_performance'))
+        // $stats[] = Stat::make(__('accounting::partner.widgets.average_payment_time'), $avgPaymentDays . ' ' . __('accounting::partner.widgets.days'))
+        //     ->description(__('accounting::partner.widgets.our_payment_performance'))
         //     ->descriptionIcon('heroicon-m-chart-bar')
         //     ->color($paymentTimeColor)
         //     ->extraAttributes([
@@ -66,8 +66,8 @@ class VendorFinancialWidget extends BaseWidget
 
         // Paid Last Month - simplified version
         $monthlyPaid = $partner->getMonthlyTransactionValue();
-        $stats[] = Stat::make(__('partner.widgets.paid_last_month'), $monthlyPaid->formatTo('en_US'))
-            ->description(__('partner.widgets.last_month_payments'))
+        $stats[] = Stat::make(__('accounting::partner.widgets.paid_last_month'), $monthlyPaid->formatTo('en_US'))
+            ->description(__('accounting::partner.widgets.last_month_payments'))
             ->descriptionIcon('heroicon-m-arrow-trending-down')
             ->color($monthlyPaid->isZero() ? 'gray' : 'info')
             ->extraAttributes([
