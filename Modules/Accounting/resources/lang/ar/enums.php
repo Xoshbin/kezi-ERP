53a5b93e0bd70bce09f6231b73cbe75e336f3a16
Xoshbin <xoshbin@gmail.com>
2026-01-03 16:53:37 +0300

    feat: Add multi-language support for Accounting module enums, navigation, and fiscal year.

---
+++ Modules/Accounting/resources/lang/ar/enums.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'account_type' => [
+        'receivable' => 'المدينون',
+        'bank_and_cash' => 'البنك والنقد',
+        'current_assets' => 'الأصول المتداولة',
+        'non_current_assets' => 'الأصول غير المتداولة',
+        'prepayments' => 'المدفوعات المقدمة',
+        'fixed_assets' => 'الأصول الثابتة',
+        'payable' => 'الدائنون',
+        'credit_card' => 'بطاقة الائتمان',
+        'current_liabilities' => 'الخصوم المتداولة',
+        'non_current_liabilities' => 'الخصوم غير المتداولة',
+        'equity' => 'حقوق الملكية',
+        'current_year_earnings' => 'أرباح العام الحالي',
+        'income' => 'الدخل',
+        'other_income' => 'دخل آخر',
+        'expense' => 'المصروفات',
+        'depreciation' => 'الإهلاك',
+        'cost_of_revenue' => 'تكلفة الإيرادات',
+        'off_balance_sheet' => 'خارج الميزانية العمومية',
+    ],
+    'fiscal_year_state' => [
+        'draft' => 'مسودة',
+        'open' => 'مفتوحة',
+        'closing' => 'جاري الإغلاق',
+        'closed' => 'مغلقة',
+    ],
+    'fiscal_period_state' => [
+        'open' => 'مفتوحة',
+        'closed' => 'مغلقة',
+    ],
+];
