285d523ad4beb0ad38e9b691a85399820f0b70e1
Xoshbin <xoshbin@gmail.com>
2026-01-04 22:44:05 +0300

    Add missing create button to FiscalYear list page

--- Modules/Accounting/app/Filament/Clusters/Accounting/Resources/FiscalYears/Pages/ListFiscalYears.php
+++ Modules/Accounting/app/Filament/Clusters/Accounting/Resources/FiscalYears/Pages/ListFiscalYears.php
@@ -2,10 +2,21 @@
 
 namespace Modules\Accounting\Filament\Clusters\Accounting\Resources\FiscalYears\Pages;
 
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Modules\Accounting\Filament\Clusters\Accounting\Resources\FiscalYears\FiscalYearResource;
 
 class ListFiscalYears extends ListRecords
 {
     protected static string $resource = FiscalYearResource::class;
+
+    /**
+     * @return array<\Filament\Actions\Action>
+     */
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
