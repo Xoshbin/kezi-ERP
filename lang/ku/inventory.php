acf682a7d32e426b04df71bc57cdd547bcbade36
Xoshbin <xoshbin@gmail.com>
2026-01-07 16:19:37 +0300

    feat: Introduce serial number tracking with Filament UI, warranty widget, and new localization for inventory.

---
+++ lang/ku/inventory.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    // جۆرەکانی شوێنکەوتن
+    'tracking_type' => 'جۆری شوێنکەوتن',
+    'tracking_type_help' => 'جۆری شوێنکەوتنی کاڵا هەڵبژێرە',
+    'tracking_type_immutable_help' => 'ناتوانرێت جۆری شوێنکەوتن بگۆڕدرێت دوای جوڵەکانی کۆگا',
+    'tracking_type_none' => 'بێ شوێنکەوتن',
+    'tracking_type_lot' => 'بە گروپەکان',
+    'tracking_type_serial' => 'بە ژمارەی زنجیرەیی',
+
+    // ژمارە زنجیرەییەکان
+    'serial_number' => 'ژمارەی زنجیرەیی',
+    'serial_numbers' => 'ژمارە زنجیرەییەکان',
+    'serial_code' => 'کۆدی زنجیرەیی',
+    'warranty_start' => 'دەستپێکی گەرەنتی',
+    'warranty_end' => 'کۆتایی گەرەنتی',
+    'warranty_status' => 'دۆخی گەرەنتی',
+    'under_warranty' => 'لە ژێر گەرەنتیدا',
+    'warranty_expired' => 'گەرەنتی بەسەرچووە',
+    'notes' => 'تێبینیەکان',
+    'current_location' => 'شوێنی ئێستا',
+    'sold_to' => 'فرۆشراوە بۆ',
+    'sold_at' => 'کاتی فرۆشتن',
+
+    // دۆخەکانی ژمارە زنجیرەییەکان
+    'serial_status_available' => 'بەردەستە',
+    'serial_status_reserved' => 'گیراوەتەوە',
+    'serial_status_sold' => 'فرۆشراوە',
+    'serial_status_returned' => 'گەڕاوەتەوە',
+    'serial_status_defective' => 'کەموکوڕی',
+
+    // کردارەکان
+    'mark_defective' => 'نیشانکردن وەک کەموکوڕی',
+    'mark_returned' => 'نیشانکردن وەک گەڕاوەتەوە',
+    'view_history' => 'بینینی مێژوو',
+
+    // زیادە
+    'serial_number_information' => 'زانیاری ژمارەی زنجیرەیی',
+];
