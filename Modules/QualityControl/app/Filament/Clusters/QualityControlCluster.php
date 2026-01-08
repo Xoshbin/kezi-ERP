d1a3e66e7520dc239cd93dbca304be1725fe6a24
Xoshbin <xoshbin@gmail.com>
2026-01-08 18:37:14 +0300

    Add Filament UI for Quality Control module

---
+++ Modules/QualityControl/app/Filament/Clusters/QualityControlCluster.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\QualityControl\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class QualityControlCluster extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-shield-check';
+
+    protected static ?int $navigationSort = 50;
+
+    public static function getNavigationLabel(): string
+    {
+        return __('quality::cluster.label');
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('quality::cluster.group');
+    }
+}
