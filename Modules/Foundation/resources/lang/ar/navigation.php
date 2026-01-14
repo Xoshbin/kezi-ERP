a3d0c93e3ee49c063fbccd5f9e546053702afb30
Xoshbin <xoshbin@gmail.com>
2026-01-14 21:18:03 +0300

    centralize settings resources across modules into SettingsCluster

---
+++ Modules/Foundation/resources/lang/ar/navigation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'groups' => [
+        'general_settings' => 'عام',
+    ],
+    'clusters' => [
+        'settings' => 'الإعدادات',
+    ],
+];
---
+++ Modules/HR/resources/lang/ar/navigation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'groups' => [
+        'hr_settings' => 'الموارد البشرية',
+    ],
+];
