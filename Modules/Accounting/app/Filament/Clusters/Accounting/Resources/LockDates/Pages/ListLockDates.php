64ceeafbd6746c71256bfa20c974db2a642fd0e7
Xoshbin <xoshbin@gmail.com>
2026-01-15 16:58:35 +0300

    Add comprehensive Lock Dates user documentation

--- Modules/Accounting/app/Filament/Clusters/Accounting/Resources/LockDates/Pages/ListLockDates.php
+++ Modules/Accounting/app/Filament/Clusters/Accounting/Resources/LockDates/Pages/ListLockDates.php
@@ -14,6 +14,7 @@ class ListLockDates extends ListRecords
     {
         return [
             CreateAction::make(),
+            \Modules\Foundation\Filament\Actions\DocsAction::make('lock-dates'),
         ];
     }
 }
