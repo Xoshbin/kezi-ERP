a0d3e692f41fbf5cde53dd366c9fa7ed6d10e21b
Xoshbin <xoshbin@gmail.com>
2026-01-15 19:53:09 +0300

    docs: add project invoicing guide with Kurdish translation

--- Modules/ProjectManagement/app/Filament/Clusters/ProjectManagement/Resources/ProjectInvoices/Pages/ListProjectInvoices.php
+++ Modules/ProjectManagement/app/Filament/Clusters/ProjectManagement/Resources/ProjectInvoices/Pages/ListProjectInvoices.php
@@ -14,6 +14,7 @@ class ListProjectInvoices extends ListRecords
     {
         return [
             CreateAction::make(),
+            \Modules\Foundation\Filament\Actions\DocsAction::make('understanding-project-invoicing'),
         ];
     }
 }
