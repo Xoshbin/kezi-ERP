b3d85a87f9ca61b9b53cfca7f41c05b2465c8b72
Xoshbin <xoshbin@gmail.com>
2026-01-18 22:36:12 +0300

    feat: Configure a dedicated SQLite database for testing, enable foreign key constraints for SQLite tests, add extensive debugging logs, and refine browser tests for invoice creation.

--- Modules/Purchase/app/Filament/Clusters/Purchases/Resources/PurchaseOrders/Pages/CreatePurchaseOrder.php
+++ Modules/Purchase/app/Filament/Clusters/Purchases/Resources/PurchaseOrders/Pages/CreatePurchaseOrder.php
@@ -14,13 +14,23 @@ class CreatePurchaseOrder extends CreateRecord
 
     protected function handleRecordCreation(array $data): Model
     {
+        \Illuminate\Support\Facades\Log::info('DEBUG_CREATE_PO: Starting handleRecordCreation', ['data_keys' => array_keys($data)]);
+
         // Add tenant company_id and current user_id to the data
         $data['company_id'] = filament()->getTenant()->getKey();
         $data['created_by_user_id'] = auth()->id();
 
+        \Illuminate\Support\Facades\Log::info('DEBUG_CREATE_PO: Prepared data', ['company_id' => $data['company_id'], 'user_id' => $data['created_by_user_id']]);
+
         $dto = CreatePurchaseOrderDTO::fromArray($data);
 
-        return app(CreatePurchaseOrderAction::class)->execute($dto);
+        \Illuminate\Support\Facades\Log::info('DEBUG_CREATE_PO: Executing Action');
+
+        $result = app(CreatePurchaseOrderAction::class)->execute($dto);
+
+        \Illuminate\Support\Facades\Log::info('DEBUG_CREATE_PO: Action successful', ['id' => $result->id]);
+
+        return $result;
     }
 
     protected function getRedirectUrl(): string
