9ab45635797bf606df8ece24d93ef36d15ac0663
Xoshbin <xoshbin@gmail.com>
2026-01-24 23:55:42 +0300

    implement AttendanceResource and tests

---
+++ Modules/HR/app/Filament/Clusters/HumanResources/Resources/Attendances/Pages/CreateAttendance.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\HR\Filament\Clusters\HumanResources\Resources\Attendances\Pages;
+
+use Filament\Facades\Filament;
+use Filament\Resources\Pages\CreateRecord;
+use Modules\HR\Filament\Clusters\HumanResources\Resources\Attendances\AttendanceResource;
+
+class CreateAttendance extends CreateRecord
+{
+    protected static string $resource = AttendanceResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['company_id'] = Filament::getTenant()->id;
+
+        return $data;
+    }
+}
