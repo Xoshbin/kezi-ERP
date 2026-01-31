dc4c2e87aecfefcf629d502ce49ac58535b0c9d5
Xoshbin <xoshbin@gmail.com>
2026-01-31 13:39:40 +0300

    upgrade laravel boost to v2

--- _ide_helper.php
+++ _ide_helper.php
@@ -33639,11 +33639,6 @@ namespace  {
 }
 
 
-namespace Facades\Livewire\Features\SupportFileUploads {
-    /**
-     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
-    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
-}
 
 
 
