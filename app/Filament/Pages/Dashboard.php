0d9e9c8e679f1ba70e4c2c3af46adc6012703ee5
Xoshbin <xoshbin@gmail.com>
2026-02-02 14:53:41 +0300

    restore dashboard page and make accounting dashboard default

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Kezi\Accounting\Filament\Clusters\Accounting\Pages\Dashboard as AccountingDashboard;
+
+class Dashboard extends AccountingDashboard
+{
+    protected static ?string $cluster = null;
+}
