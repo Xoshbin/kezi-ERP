3e41cf227ac9ea183f6515ac8aa9e8c0cea45991
Xoshbin <xoshbin@gmail.com>
2026-02-24 10:50:52 +0300

    Refactor generateUUID to use const and standard UUID fallbacks

--- packages/kezi/pos/resources/js/App.vue
+++ packages/kezi/pos/resources/js/App.vue
@@ -410,17 +410,22 @@ const generateUUID = () => {
         }
         
         if (typeof crypto !== 'undefined' && crypto.getRandomValues) {
-            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
                 (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
             );
         }
 
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            const r = Math.random() * 16 | 0;
+            const v = c === 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     } catch (e) {
-        return 'pos-' + Date.now() + '-' + Math.floor(Math.random() * 1000000);
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            const r = Math.random() * 16 | 0;
+            const v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
 };
 
